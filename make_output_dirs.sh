dc84f67786de36dcbe1503868fb8a72682f7d1d3
Sam Thomson <sp.thomson@outlook.com>
2016-04-01 11:51:36 +0100

    Updated default output directories.

--- make_output_dirs.sh
+++ make_output_dirs.sh
@@ -1,6 +1,5 @@
-mkdir ../sphray_output
-mkdir ../sphray_output/IT1_N64
-mkdir ../sphray_output/IT1_N64/r6
-mkdir ../sphray_output/IT2_N64
-mkdir ../sphray_output/IT2_N64/r6
-mkdir ../sphray_output/IT1_He
+mkdir -p ../sphray_output/IT1_N128/r6
+mkdir -p ../sphray_output/IT1_He_N128/r6
+mkdir -p ../sphray_output/IT2_N128/r6
+mkdir -p ../sphray_output/IT3/r6
+
